6e4e12ff0d839a9227bc6892d83ecf627b37c7dd
lishu gupta <lishugupta652@gmail.com>
2020-07-15 20:07:13 +0530

    final

--- routes/posts.js
+++ routes/posts.js
@@ -36,5 +36,33 @@ router.post("/", async (req, res) => {
 });
 
 // SPECIFIC POST
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
+// Delete Post
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Update Post
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {}
+});
 module.exports = router;
